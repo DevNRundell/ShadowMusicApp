9a7f754dde189113b63b357c12e05160a2f2fca9
DevNRundell <nickrundelL@live.com>
2017-08-01 20:36:25 -0400

    Create DBUtils.java

---
+++ Database/DBUtils.java
@@ -0,0 +1,55 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/*
+ * Utilities for closing database functionality.  
+ */
+
+public class DBUtils {
+
+    public static void closeConn(Connection connection) {
+
+        if (connection != null) {
+
+            try {
+
+                connection.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeStatement(PreparedStatement statement) {
+
+        if (statement != null) {
+
+            try {
+
+                statement.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet resultSet) {
+
+        if (resultSet != null) {
+
+            try {
+
+                resultSet.close();
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
